c448e85cb5c978dfa76a0de78f504db84d12fa5b
Joshua Hull <joshbuddy@gmail.com>
2009-11-20 19:04:49 -0800

    much simpler notation

--- lib/dirge.rb
+++ lib/dirge.rb
@@ -3,7 +3,7 @@ module Kernel
     called_from ||= caller.first
     caller_path = called_from[/(.*?(?=:\d))/, 1]
     caller_path = '.' if caller_path == ''
-    File.dirname(caller_path)
+    File.expand_path(File.dirname(caller_path))
   end
 
   def require_relative(path)
@@ -15,5 +15,11 @@ end
 class Module
   def self.autoload_relative(name, filename)
     autoload name, File.join(__DIR__(caller.first), filename)
+  end
+end
+
+class String
+  def ~@
+    File.expand_path(File.join(__DIR__(caller.first), self))
   end
 end
